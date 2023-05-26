aae4016c0f455084c8e8ad297696ecc9621d2da1
ThibaultBee <37510686+ThibaultBee@users.noreply.github.com>
2023-05-26 10:33:52 +0200

    feat(write): add WriteWithMsgCtrl

--- write.go
+++ write.go
@@ -13,11 +13,13 @@ int srt_sendmsg2_wrapped(SRTSOCKET u, const char* buf, int len, SRT_MSGCTRL *mct
 	return ret;
 }
 
+
 */
 import "C"
 import (
 	"errors"
 	"syscall"
+	"time"
 	"unsafe"
 )
 
@@ -51,5 +53,34 @@ func (s SrtSocket) Write(b []byte) (n int, err error) {
 		}
 		s.pd.wait(ModeWrite)
 		n, err = srtSendMsg2Impl(s.socket, b, nil)
+	}
+}
+
+// WriteWithSrcTime data to the SRT socket with msgctrl
+func (s SrtSocket) WriteWithSrcTime(b []byte, srcTime time.Duration) (n int, err error) {
+	msgctrl := C.SRT_MSGCTRL{
+		flags:        0, // no flags set
+		msgttl:       C.SRT_MSGTTL_INF,
+		inorder:      0, // false - not in order (matters for msg mode only)
+		boundary:     0,
+		srctime:      C.longlong(srcTime / 1000), // srctime: from ns to us
+		pktseq:       C.SRT_SEQNO_NONE,
+		msgno:        C.SRT_MSGNO_NONE,
+		grpdata:      nil, // grpdata not supplied
+		grpdata_size: 0,
+	}
+
+	//Fastpath:
+	if !s.blocking {
+		s.pd.reset(ModeWrite)
+	}
+	n, err = srtSendMsg2Impl(s.socket, b, &msgctrl)
+
+	for {
+		if !errors.Is(err, error(EAsyncSND)) || s.blocking {
+			return
+		}
+		s.pd.wait(ModeWrite)
+		n, err = srtSendMsg2Impl(s.socket, b, &msgctrl)
 	}
 }
